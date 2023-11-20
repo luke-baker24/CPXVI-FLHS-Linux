e005abdeb018809d779a344c36f65b898a36d7de
EpicCrownKing <epiccrownking@gmail.com>
2023-11-19 19:33:58 -0800

    fix

--- subscripts/scans/files.sh
+++ subscripts/scans/files.sh
@@ -49,19 +49,40 @@ aide_scan general $logs_directory
 
 #Get added files
 #first command img in discord
-for added_file in $(cat $logs_directory/policy-aide.log | grep -E "^[^d][\+]{17}" | awk 'BEGIN { FS = ": " } ; {print $2}' | grep -vE $(cat $logs_directory/policy-aide.log | grep -E "^d.{17}\: .*$" | awk 'BEGIN { FS = ": " } ; {print $2}' | tr '\n' '|')); do
+for added_file in $(cat $logs_directory/policy-aide.log | grep -E "^[^d][\+]{17}" | awk 'BEGIN { FS = ": " } ; {print $2}' | grep -vE $(cat $logs_directory/policy-aide.log | grep -E "^d.{17}\: .*$" | awk 'BEGIN { FS = ": " } ; {print $2}' | tr '\n' '|' | uniq)); do
     output_log "FIL" "$added_file has been added"
 done
 
 #Get permissions modified
-for changed_file in $(cat $logs_directory/policy-aide.log | grep -E "^.{4}p.{13}\: " | awk 'BEGIN { FS = ": " } ; {print $2}'); do
-    output_log "PRM" "$changed_file has changed file permissions from $(cat $logs_directory/policy-aide.log | grep -E "^.*: $changed_file$" -A 10 | grep 'Perm' | cut -d ':' -f 2 | cut -d ' ' -f 2 | head -1) to $(cat $logs_directory/policy-aide.log | grep -E "^.*: $changed_file$" -A 10 | grep 'Perm' | cut -d '|' -f 2 | cut -d ' ' -f 2 | head -1)"
+for changed_file in $(cat $logs_directory/policy-aide.log | grep -E "^.{4}p.{13}\: " | awk 'BEGIN { FS = ": " } ; {print $2}' | uniq); do
+    perms_line=$(cat $logs_directory/policy-aide.log | grep -E "^.*: $changed_file$" -A 10 | grep 'Perm')
+
+    old_perms=$(echo $perms_line | cut -d ':' -f 2 | cut -d ' ' -f 2 | head -1)
+    new_perms=$(echo $perms_line | cut -d '|' -f 2 | cut -d ' ' -f 2 | head -1)
+
+    output_log "PRM" "$changed_file has changed file permissions from $old_perms to $new_perms"
 done
 
-for changed_file in $(cat $logs_directory/policy-aide.log | grep -E "^.{5}u.{12}\: " | awk 'BEGIN { FS = ": " } ; {print $2}'); do
-    output_log "PRM" "$changed_file has changed user ownership from $(id -nu $(cat $logs_directory/policy-aide.log | grep -E "^.*: $changed_file$" -A 10 | grep 'Uid' | cut -d ':' -f 2 | cut -d ' ' -f 2 | head -1)) to $(id -nu $(cat $logs_directory/policy-aide.log | grep -E "^.*: $changed_file$" -A 10 | grep 'Uid' | cut -d '|' -f 2 | cut -d ' ' -f 2 | head -1))"
+for changed_file in $(cat $logs_directory/policy-aide.log | grep -E "^.{5}u.{12}\: " | awk 'BEGIN { FS = ": " } ; {print $2}' | uniq); do
+    uid_line=$(cat $logs_directory/policy-aide.log | grep -E "^.*: $changed_file$" -A 10 | grep 'Uid')
+
+    old_uid=$(echo $uid_line | cut -d ':' -f 2 | cut -d ' ' -f 2 | head -1)
+    new_uid=$(echo $uid_line | cut -d '|' -f 2 | cut -d ' ' -f 2 | head -1)
+
+    old_user=$(id -nu $old_uid 2> /dev/null) 
+    new_user=$(id -nu $new_uid 2> /dev/null)
+
+    output_log "PRM" "$changed_file has changed user ownership from $old_uid $old_user to $new_uid $new_user"
 done
 
-for changed_file in $(cat $logs_directory/policy-aide.log | grep -E "^.{6}g.{11}\: " | awk 'BEGIN { FS = ": " } ; {print $2}'); do
-    output_log "PRM" "$changed_file has changed group ownership from $(id -nu $(cat $logs_directory/policy-aide.log | grep -E "^.*: $changed_file$" -A 10 | grep 'Gid' | cut -d ':' -f 2 | cut -d ' ' -f 2 | head -1)) to $(id -nu $(cat $logs_directory/policy-aide.log | grep -E "^.*: $changed_file$" -A 10 | grep 'Gid' | cut -d '|' -f 2 | cut -d ' ' -f 2 | head -1))"
+for changed_file in $(cat $logs_directory/policy-aide.log | grep -E "^.{6}g.{11}\: " | awk 'BEGIN { FS = ": " } ; {print $2}' | uniq); do
+    uid_line=$(cat $logs_directory/policy-aide.log | grep -E "^.*: $changed_file$" -A 10 | grep 'Gid')
+
+    old_uid=$(echo $uid_line | cut -d ':' -f 2 | cut -d ' ' -f 2 | head -1)
+    new_uid=$(echo $uid_line | cut -d '|' -f 2 | cut -d ' ' -f 2 | head -1)
+
+    old_user=$(id -nu $old_uid 2> /dev/null) 
+    new_user=$(id -nu $new_uid 2> /dev/null)
+
+    output_log "PRM" "$changed_file has changed group ownership from $old_uid $old_user to $new_uid $new_user"
 done

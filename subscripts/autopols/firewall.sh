82ff7cddae9456335469cb9943b3a64e88d851d0
EpicCrownKing <epiccrownking@gmail.com>
2023-11-25 12:17:48 -0800

    push

--- subscripts/autopols/firewall.sh
+++ subscripts/autopols/firewall.sh
@@ -13,6 +13,8 @@ ufw default deny incoming
 ufw default allow outgoing #should be deny according to CIS but that literally denies everything
 ufw default deny routed
 
+ufw logging medium
+
 #note: configure more ports, also 3.5.1.7 may make backdoors harder to find
 #ufw allow git
 #ufw allow in http
